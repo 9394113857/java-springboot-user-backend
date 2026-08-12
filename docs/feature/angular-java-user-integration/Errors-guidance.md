c248ec7476f9d4470d4d493ff13225e2a81b90c3
Raghunadh_9394113857 <barlapudiraghunadh9@gmail.com>
2026-08-12 13:46:58 +0530

    feat: add user entity and repository

---
+++ docs/feature/angular-java-user-integration/Errors-guidance.md
@@ -0,0 +1,5 @@
+<!--  -->
+.\mvnw.cmd test 2>&1 | Select-String -Pattern "Caused by:|Exception:|Error:" -Context 0,5
+
+.\mvnw.cmd test
+
--- src/main/java/com/javauserbackend/User.java
+++ src/main/java/com/javauserbackend/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     private String username;
 
@@ -26,11 +26,11 @@ public class User {
         this.email = email;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
